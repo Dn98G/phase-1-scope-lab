81689b3f260e92ffff9e3d6357667b7121d8f778
Denis Cheruiyot <Cheruiyotkp974@gmail.com>
2025-03-17 19:13:50 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+customerName = "bob";
+console.log(customerName);
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName);
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+bestCustomer;
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+  return bestCustomer;
+}
+console.log(overwriteBestCustomer());
+
+const leastFavoriteCustomer = "doe";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Jane";
+  return leastFavoriteCustomer;
+}
